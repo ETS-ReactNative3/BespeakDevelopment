51b91a6b683f56318261f52a61c1d1bc48359ee6
--replace-all <wardpedregosa@gmail.com>
2022-02-23 01:44:25 +0800

    [FIX] Edit Profile Not Uploading

--- screens/FollowListScreen.js
+++ screens/FollowListScreen.js
@@ -9,53 +9,8 @@ class FollowingScreen extends Component {
     render() {
         return (
             <View style={SystemStyle.Container}>
-
                 <OrganizerList list_following = {true}
                     navigation = { this.props.navigation }/>
-    /*
-                <View style={SystemStyle.FollowOrganizerTab}>
-                    <TouchableOpacity style={SystemStyle.OrganizerInfo}
-                    //onPress={() => navigation.navigate(' ')}
-                    >
-                        <View style={SystemStyle.OrganizerImgContainer}>
-                            <Image
-                                style={SystemStyle.FollowOrganizerImg}
-                                source={require('../assets/img/EveryNation.png')}
-                            />
-                        </View>
-                        <View style={SystemStyle.FollowCardContainer}>
-                            <Text style={SystemStyle.FollowListOrganizerName}>Every Nation Campus</Text>
-                            <Text style={SystemStyle.FollowListOrganizerBio}>Bio sabi mo eto para sayo hakhak.Bio sabi mo eto para sayo hakhak</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={SystemStyle.ToFollowingOrgBtn}
-                        //onPress={() => navigation.navigate('')}
-                    >
-                        <Text style={SystemStyle.FollowOrgTextBtn}>Following</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={SystemStyle.FollowOrganizerTab}>
-                    <TouchableOpacity style={SystemStyle.OrganizerInfo}
-                    //onPress={() => navigation.navigate(' ')}
-                    >
-                        <View style={SystemStyle.OrganizerImgContainer}>
-                            <Image
-                                style={SystemStyle.FollowOrganizerImg}
-                                source={require('../assets/img/EveryNation.png')}
-                            />
-                        </View>
-                        <View style={SystemStyle.FollowCardContainer}>
-                            <Text style={SystemStyle.FollowListOrganizerName}>Every Nation Campus</Text>
-                            <Text style={SystemStyle.FollowListOrganizerBio}>Bio sabi mo eto para sayo hakhak.Bio sabi mo eto para sayo hakhak</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={SystemStyle.ToFollowingOrgBtn}
-                        //onPress={() => navigation.navigate('')}
-                    >
-                        <Text style={SystemStyle.FollowOrgTextBtn}>Following</Text>
-                    </TouchableOpacity>
-                </View>
-*/
             </View>
         );
     }
@@ -67,50 +22,6 @@ class FollowerScreen extends Component {
             <View style={SystemStyle.Container}>
                 <OrganizerList list_follower = {true}
                     navigation = { this.props.navigation }/>
-  /*
-                <View style={SystemStyle.FollowOrganizerTab}>
-                    <TouchableOpacity style={SystemStyle.OrganizerInfo}
-                    //onPress={() => navigation.navigate(' ')}
-                    >
-                        <View style={SystemStyle.OrganizerImgContainer}>
-                            <Image
-                                style={SystemStyle.FollowOrganizerImg}
-                                source={require('../assets/img/EveryNation.png')}
-                            />
-                        </View>
-                        <View style={SystemStyle.FollowCardContainer}>
-                            <Text style={SystemStyle.FollowListOrganizerName}>Every Nation Campus</Text>
-                            <Text style={SystemStyle.FollowListOrganizerBio}>Bio sabi mo eto para sayo hakhak.Bio sabi mo eto para sayo hakhak</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={SystemStyle.ToFollowOrgBtn}
-                        //onPress={() => navigation.navigate('')}
-                    >
-                        <Text style={SystemStyle.FollowOrgTextBtn}>Follow</Text>
-                    </TouchableOpacity>
-                </View>
-                <View style={SystemStyle.FollowOrganizerTab}>
-                    <TouchableOpacity style={SystemStyle.OrganizerInfo}
-                    //onPress={() => navigation.navigate(' ')}
-                    >
-                        <View style={SystemStyle.OrganizerImgContainer}>
-                            <Image
-                                style={SystemStyle.FollowOrganizerImg}
-                                source={require('../assets/img/EveryNation.png')}
-                            />
-                        </View>
-                        <View style={SystemStyle.FollowCardContainer}>
-                            <Text style={SystemStyle.FollowListOrganizerName}>Every Nation Campus</Text>
-                            <Text style={SystemStyle.FollowListOrganizerBio}>Bio sabi mo eto para sayo hakhak.Bio sabi mo eto para sayo hakhak</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={SystemStyle.ToFollowOrgBtn}
-                        //onPress={() => navigation.navigate('')}
-                    >
-                        <Text style={SystemStyle.FollowOrgTextBtn}>Follow</Text>
-                    </TouchableOpacity>
-                </View>
-*/
             </View>
         );
     }
